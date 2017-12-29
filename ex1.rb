32502ea4916fa90af70d405737ec5c78748f409a
paulcosta2001@gmail.com <paulcosta2001@gmail.com>
2017-12-29 18:05:41 -0500

    ex1.rb update

---
+++ ex1.rb
@@ -0,0 +1,38 @@
+def letter_grade(percentage)
+
+if percentage    = 95
+return "A+"
+
+elsif percentage = 90
+return "A-"
+
+elsif percentage = 85
+return "B+"
+
+elsif percentage = 80
+return "B-"
+
+elsif percentage = 75
+return "C+"
+
+elsif percentage = 70
+return "C-"
+
+elsif percentage = 65
+return "D+"
+
+elsif percentage = 60
+return "D-"
+
+
+elsif percentage = 55
+return "E+"
+
+elsif percentage = 50
+return "E-"
+  
+else
+return "F"
+
+end
+end
